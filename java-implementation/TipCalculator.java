0179f4ba337ed871738bfb45074e940efa381222
Ryan <ryan.samarajeewa@gmail.com>
2015-06-21 13:45:18 -0700

    Initial commit

---
+++ java-implementation/TipCalculator.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class TipCalculator {
+	public static void main(String[]args){
+		Scanner userInput = new Scanner(System.in);
+	}
+
+	private double getTipPercent(){
+		return 0;
+	}
+
+	private int getBill(){
+		return 0;
+	}
+
+	private void displayPrices(int bill, double tip){
+
+	}
+}
